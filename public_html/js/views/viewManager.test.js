00eac049ee40d279b5bcbca0a6444b7f5c4cf60a
Dmitry Orlov <feanarokurufinve@mail.ru>
2016-04-01 21:49:46 +0300

    fix tests

--- public_html/js/views/viewManager.test.js
+++ public_html/js/views/viewManager.test.js
@@ -13,24 +13,16 @@ define(function (require) {
     });
 
     QUnit.test('viewManager adds views', function (assert) {
-        var mainView = require('views/main'),
-            loginView = require('views/login'),
-            gameView = require('views/game'),
+        var gameView = require('views/game'),
             scoreboardView = require('views/scoreboard'),
             viewManager = require('views/viewManager');
 
-        viewManager.addView(mainView);
-        assert.notOk(viewManager.views.length != 1 || viewManager.views[viewManager.views.length - 1] != mainView, 'First addition ok');
-
-        viewManager.addView(loginView);
-        assert.notOk(viewManager.views.length != 2 || viewManager.views[viewManager.views.length - 1] != loginView, 'Second addition ok');
-
         viewManager.addView(gameView);
-        assert.notOk(viewManager.views.length != 3 || viewManager.views[viewManager.views.length - 1] != gameView, 'Third addition ok');
+        assert.notOk(viewManager.views.length != 2 || viewManager.views[viewManager.views.length - 1] != gameView, 'Second addition ok');
 
         viewManager.addView(scoreboardView);
-        assert.notOk(viewManager.views.length != 4 || viewManager.views[viewManager.views.length - 1] != scoreboardView, 'Fourth addition ok');
+        assert.notOk(viewManager.views.length != 3 || viewManager.views[viewManager.views.length - 1] != scoreboardView, 'Third addition ok');
 
     });
-    
+
 });
