bcef7b21233edff58b6097637538231cbfda18a9
Orlov D <feanarokurufinve@mail.ru>
2016-04-05 00:16:09 +0300

    copypaste issue

--- public_html/js/views/login.js
+++ public_html/js/views/login.js
@@ -19,29 +19,21 @@ define([
 
         showError: function (container) {
             container.addClass('form__error_shown');
-            var msgElem = container.children('.form__error');
-            var formElem = container.children('.form__input');
-            msgElem.addClass('form__error_shown');
-            formElem.addClass('form__input_error');
+            container.children('.form__error').addClass('form__error_shown');
+            container.children('.form__input').addClass('form__input_error');
         },
 
-        resetError: function (container) {
-            container.removeClass('form__error_shown');
-            var msgElem = container.children('.form__error');
-            var formElem = container.children('.form__input');
-            msgElem.removeClass('form__error_shown');
-            formElem.removeClass('form__input_error');
+        resetErrors: function () {
+            this.$el.find('.form__error_shown').removeClass('form__error_shown');
+            this.$el.find('.form__input_error').removeClass('form__input_error');
         },
 
         handleErrors: function(model, error) {
-            console.log(error);
-            this.resetError(this.$el.find('.form__username'));
-            this.resetError(this.$el.find('.form__password'));
-            if (error.username) {
-                this.showError(this.$el.find('.form__username'));
-            }
-            if (error.password) {
-                this.showError(this.$el.find('.form__password'));
+            this.resetErrors();
+            for (var field in this.erratives) {
+                if (error[this.erratives[field]]) {
+                    this.showError(this.$el.find('.form__' + this.erratives[field]));
+                }
             }
         },
 
@@ -50,8 +42,7 @@ define([
                 event.preventDefault();
             }
 
-            var nativeForm = $('form')[0]
-            this.elems = nativeForm.elements;
+            this.elems = $('form')[0].elements;
 
             this.model.save({
                 'username': this.elems.username.value,
@@ -75,8 +66,7 @@ define([
         },
         hide: function () {
             this.$el.hide();
-            this.resetError(this.$el.find('.form__username'));
-            this.resetError(this.$el.find('.form__password'));
+            this.resetErrors();
         },
 
     });
