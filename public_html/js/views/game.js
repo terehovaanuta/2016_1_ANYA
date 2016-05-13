598105591076002c236c85f4a414138cc24b2b5b
Orlov Dmitry <feanarokurufinve@mail.ru>
2016-05-13 19:20:02 +0300

    am i an idiot?

--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -33,33 +33,6 @@ define([
         },
 
         game: function() {
-            function throttle(fn, threshhold, scope) {
-                if (!threshhold) {
-                    threshhold = 250;
-                }
-                if (!scope) {
-                    scope = this;
-                }
-                var last,
-                    deferTimer;
-                return function () {
-                    var context = scope;
-
-                    var now = +new Date,
-                    args = arguments;
-                    if (last && now < last + threshhold) {
-                        // hold on to it
-                        clearTimeout(deferTimer);
-                        deferTimer = setTimeout(function () {
-                            last = now;
-                            fn.apply(context, args);
-                        }, threshhold);
-                    } else {
-                        last = now;
-                        fn.apply(context, args);
-                    }
-                };
-            }
 
             var renderer = PIXI.autoDetectRenderer($(window).width() * 0.8, $(window).height() * 0.7, {view: this.$el.find('#game-canvas').get(0)});
 
